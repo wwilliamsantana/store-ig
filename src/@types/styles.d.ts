fa57e399e2b856fe3f0e420a2241e749c2f321d2
William Santana <euwilliamsantana@gmail.com>
2025-02-26 12:00:36 -0300

    feat: Fixing styles.d.ts

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -3,7 +3,6 @@ import { defaultTheme } from '../styles/theme/default'
 
 type ThemeType = typeof defaultTheme
 
-
-declare module "styled-components" {
-  export interface DefaultTheme extends ThemeType
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType { }
 }
