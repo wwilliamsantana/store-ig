5bcc24efcda686ebd271c62372743e91379b8f3a
William Santana <euwilliamsantana@gmail.com>
2025-02-26 12:05:44 -0300

    feat: fixing configuration vite.config.ts part 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/store-ig/',
+  base: 'https://wwilliamsantana.github.io/store-ig//',
 })
